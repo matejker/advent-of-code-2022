f5f7d4c3592c69c034d330b1d09e5f74fc0f62f8
Matej Kerekrety <matejkerekrety@datavant.com>
2022-12-06 08:21:09 +0000

    Some freudian one line solution

--- 06/06.py
+++ 06/06.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-def marker_detector(file = None, n = 4):
+def marker_detector(file=None, n=4):
     file = file or Path(__file__).parent / "input.txt"
 
     with open(file) as f:
@@ -13,8 +13,17 @@ def marker_detector(file = None, n = 4):
                     break
 
 
+def marker_detector_one_liner(file=Path(__file__).parent / "input.txt", n=4):
+    with open(file) as f:
+        data = f.read()
+        return [next((i + n for i in range(len(l)) if len(set(l[i:i + n])) == n)) for l in data.split("\n")]
+
+
 test_file = Path(__file__).parent / "test.txt"
 assert list(marker_detector(test_file)) == [7, 5, 6, 10, 11]
 assert list(marker_detector(test_file, n=14)) == [19, 23, 23, 29, 26]
+
+assert marker_detector_one_liner(test_file) == [7, 5, 6, 10, 11]
+assert marker_detector_one_liner(test_file, n=14) == [19, 23, 23, 29, 26]
 
 print(list(marker_detector(n=14)))
